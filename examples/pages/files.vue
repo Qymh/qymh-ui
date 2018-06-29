d6f2d93495ec75439a39a789a1c2dc233ca4a9f3
Qymh <415570450@qq.com>
2018-06-29 15:57:04 +0800

    complete q-files

--- examples/pages/files.vue
+++ examples/pages/files.vue
@@ -9,13 +9,39 @@
         centerText="q-files"
         :rightEmpty="true")
     q-row(h=12)
-    q-files(color="#a1a1a1")
+    q-row
+      q-files(ref="myFiles" @getFiles="getFiles" color="#a1a1a1")
+    q-row(dir="left" wrap="wrap")
+      q-row(
+        position="relative"
+        v-for="(img,index) in images"
+        :key="index")
+        q-col(
+          tag="img" h=30 w=30
+          :src="img")
+        q-row(
+          position="absolute" align="center" justify="center"
+          r=0 t=0 w=5 h=5 radius=5 bkColor="deepskyblue"
+          fontSize="12" color="white" @clicked="remove(index)")
+          q-col X
 </template>
 
 <script lang="ts">
   import {Vue,Component} from 'vue-property-decorator'
   @Component({})
   export default class ExFiles extends Vue{
+    private files:any=[]
+    private images:any=[]
 
+    private getFiles(data:any){
+      this.files=data.files
+      this.images=data.images
+    }
+
+    private remove(index:any){
+      let myFiles:any=this.$refs.myFiles
+      myFiles.files.splice(index,1)
+      myFiles.images.splice(index,1)
+    }
   }
 </script>
--- packages/files/src/main.vue
+++ packages/files/src/main.vue
@@ -3,7 +3,9 @@
     input.q_files_input(
       type="file"
       :multiple="multiple?'multiple':''"
-      :style="computedStyle")
+      :style="computedStyle"
+      @change="getFiles"
+      @click="clear")
     span.q_files_upload(
       :style="computedStyle") {{value}}
 </template>
@@ -14,9 +16,23 @@
   import createStyle from '../../proto/files'
   @Component({})
   export default class QFiles extends Proto{
+    // 本地保存的上传图片
+    private images:string[]=[]
+    
+    // 本地保存的上传formData
+    private files:any[]=[]
+
     // 是否允许多文件上传
     @Prop({default:true})
     private multiple:boolean
+
+    // 最大上传数量
+    @Prop({default:3})
+    private maxCount:number
+
+    // 最大上传大小
+    @Prop({default:4})
+    private maxSize:number
 
     // 按钮文字
     @Prop({default:'点击上传'})
@@ -54,6 +70,77 @@
       }
       let resultStyle=Object.assign({},style,selectedStyle,baseStyle)
       return resultStyle
+    }
+
+    // 点击清除
+    private clear(e:any){
+      e.target.value=''
+    }
+
+    // 获取改变后的文件
+    private async getFiles(e:any){
+      let files=e.target.files
+      if((this.files.length+files.length)>this.maxSize){
+        this.$notice.toast(`图片最多上传${this.maxSize}张!`)
+        return
+      }
+      for(let i=0,len=files.length;i<len;i++){
+        let file=files[i]
+        let bool=await this.checkFiles(file)
+        bool&&await this.compilerFiles(file)
+      }
+      this.$emit('getFiles',{files:this.files,images:this.images})
+    }
+
+    // 检查files是否符合规格
+    private checkFiles(file:any){
+      return new Promise((resolve,reject)=>{
+        let name=file.name
+        let type=file.type
+        let typeRegExp=/image\/(png|jpg|jpeg)/
+        let size=file.size
+        // 检查图片是否为空
+        if(!size&&!name){
+          this.$notice.toast('图片不能为空!')
+          resolve(false)
+        }
+        // 检查图片类型
+        if(!typeRegExp.test(type)){
+          this.$notice.toast('图片格式仅支持png jpg jpeg!')
+          resolve(false)
+        }
+        // 检查大小
+        if(size>=this.maxSize*1024*1024){
+          this.$notice.toast(`上传的图片不能大于${this.maxSize}M!`)
+          resolve(false)
+        }
+        resolve(true)
+      })
+    }
+
+    // 解析files
+    private compilerFiles(file:any){
+      return new Promise(resolve=>{
+        let image=new Image()
+        let reader=new FileReader()
+        reader.readAsDataURL(file)
+
+        for(let i=0;i<this.files.length;i++){
+          let item=this.files[i]
+          if(item.name===file.name&&item.size===file.size){
+            this.$notice.toast('不能上传相同的图片!')
+            resolve()
+            return
+          }
+        }
+
+        reader.onload=(e:any)=>{
+          let result=e.target.result
+          this.images.push(result)
+          this.files.push(file)
+          resolve()
+        }
+      })
     }
   }
 </script>
