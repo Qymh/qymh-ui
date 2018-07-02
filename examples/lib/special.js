af12d952e3ea8ae2001b4b9fd131933b1911808a
Qymh <415570450@qq.com>
2018-07-02 14:27:38 +0800

    add special.js to fixed some bug about android browser

--- examples/lib/special.js
+++ examples/lib/special.js
@@ -1,3 +1,19 @@
-Number.prototype.toFixed = function(num){  
-  return(parseInt(this * Math.pow( 10, num  ) + 0.5)/Math.pow(10,num)).toString();  
-} 
+// 安卓原生浏览器不支持的属性
+
+// 字符includes
+String.prototype.includes=function(item){
+  var str=this
+  return str.indexOf(item)>-1
+}
+
+// 数组includes
+Object.defineProperty(Array.prototype,'includes',{
+  writable:true,
+  // 强制不可枚举
+  enumerable:false,
+  configurable:true,
+  value:function(item){
+    var arr=this
+    return arr.indexOf(item)>-1
+  }
+})
