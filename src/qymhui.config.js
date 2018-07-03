99de7f449c4b2fb6f58c45ec76cd9ac254ee184c
Qymh <415570450@qq.com>
2018-07-03 15:17:58 +0800

    add qymhui.config.js

---
+++ src/qymhui.config.js
@@ -0,0 +1,12 @@
+import errorImage from './images/default.png'
+
+// q-image配置
+export const image={
+  // 懒加载配置
+  lazyLoad:{
+    preLoad:1.3,
+    loading:'',
+    error:errorImage,
+    attemp:1
+  }
+}
