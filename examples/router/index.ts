796fbf088e6f63ef681652226c50a0ba6db1402b
Qymh <415570450@qq.com>
2018-07-03 11:03:07 +0800

    more standard about router

--- examples/router/index.ts
+++ examples/router/index.ts
@@ -1,87 +1,19 @@
 import Vue,{AsyncComponent} from 'vue'
 import Router,{RouteConfig} from 'vue-router'
+import RoutesArr from './routes'
 
 Vue.use(Router)
 
-const Home:AsyncComponent=()=>import('@/pages/home.vue')
-const Text:AsyncComponent=()=>import('@/pages/text.vue')
-const Layout:AsyncComponent=()=>import('@/pages/layout.vue')
-const Cell:AsyncComponent=()=>import('@/pages/cell.vue')
-const HeadBar:AsyncComponent=()=>import('@/pages/headBar.vue')
-const SearchBar:AsyncComponent=()=>import('@/pages/searchBar.vue')
-const TabBar:AsyncComponent=()=>import('@/pages/tabBar.vue')
-const Tag:AsyncComponent=()=>import('@/pages/tag.vue')
-const Form:AsyncComponent=()=>import('@/pages/form.vue')
-const Radio:AsyncComponent=()=>import('@/pages/radio.vue')
-const Stepper:AsyncComponent=()=>import('@/pages/Stepper.vue')
-const Table:AsyncComponent=()=>import('@/pages/Table.vue')
-const Overlay:AsyncComponent=()=>import('@/pages/Overlay.vue')
-const Files:AsyncComponent=()=>import('@/pages/files.vue')
-const Swiper:AsyncComponent=()=>import('@/pages/swiper.vue')
+const routes:RouteConfig[]=[]
 
-
-const routes:RouteConfig[]=[
-  {
-    path:'/',
-    component:Home
-  },
-  {
-    path:'/layout',
-    component:Layout
-  },
-  {
-    path:'/text',
-    component:Text
-  },
-  {
-    path:'/cell',
-    component:Cell
-  },
-  {
-    path:'/headBar',
-    component:HeadBar,
-  },
-  {
-    path:'/searchBar',
-    component:SearchBar
-  },
-  {
-    path:'/tabBar',
-    component:TabBar
-  },
-  {
-    path:'/tag',
-    component:Tag
-  },
-  {
-    path:'/form',
-    component:Form
-  },
-  {
-    path:'/radio',
-    component:Radio
-  },
-  {
-    path:'/stepper',
-    component:Stepper
-  },
-  {
-    path:'/table',
-    component:Table
-  },
-  {
-    path:'/overlay',
-    component:Overlay
-  },
-  {
-    path:'/files',
-    component:Files
-  },
-  {
-    path:'/swiper',
-    component:Swiper
-  }
-]
+for(let item of RoutesArr){
+  let path=item.path?item.path:`/${item.name}`
+  let component:AsyncComponent=()=>import(`@/pages/${item.name}.vue`)
+  routes.push({
+    path:path,
+    component
+  })
+}
 
 const router:Router=new Router({
   mode:'history',
---
+++ examples/router/routes.ts
@@ -0,0 +1,38 @@
+const Routes:diyRoute.RouteConfig[]=[
+  // 首页
+  {name:'home',path:'/'},
+  // 布局 q-row q-col
+  {name:'layout'},
+  // 文本 q-text
+  {name:'text'},
+  // 块 q-cell
+  {name:'cell'},
+  // 头部导航 q-head-bar
+  {name:'headBar'},
+  // 搜索导航 q-search-bar
+  {name:'searchBar'},
+  // 底部导航 q-tab-bar
+  {name:'tabBar'},
+  // 标签 q-tag
+  {name:'tag'},
+  // 表单 q-form q-input
+  {name:'form'},
+  // 按钮 q-radio
+  {name:'radio'},
+  // 数据录入 q-stepper
+  {name:'stepper'},
+  // 表格 q-table
+  {name:'/table'},
+  // 蒙层 q-overlay
+  {name:'overlay'},
+  // 文件 q-files
+  {name:'files'},
+  // 滑动轮播 q-swiper
+  {name:'swiper'},
+  // 图片查看 q-photo
+  {name:'photo'},
+  // 滑动选择 q-select
+  {name:'select'}
+]
+
+export default Routes
