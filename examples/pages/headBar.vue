03aeee5c55195ec0c9031903da72aec732489f8b
Qymh <415570450@qq.com>
2018-07-04 12:01:06 +0800

    headBar document

--- examples/pages/headBar.vue
+++ examples/pages/headBar.vue
@@ -6,13 +6,57 @@
       :leftArrow="true"
       centerText="q-head-bar"
       :rightEmpty="true")
-    q-row(mt=2)
-      q-head-bar(
-        bkColor="#F65A44"
-        color="white"
-        leftText="返回"
-        centerText="商品"
-        rightText="购物车")
+    q-row(pl=2 pr=2)
+      //- Demo
+      q-row
+        //- demo演示
+        q-row(tag="section")
+          q-cell(
+            leftIcon="q-icon icon-tagfill"
+            leftIconColor="deepskyblue"
+            title="Demo演示"
+            fontSize=16
+            :borderBottom="true")
+          //- 简单的列子
+          q-row(tag="section")
+            q-cell(
+              leftIcon="q-icon icon-edit"
+              leftIconColor="deepskyblue"
+              title="简单的列子")
+            q-row(tag="section")
+              q-head-bar(
+                :leftArrow="true"
+                centerText="订单列表"
+                centerTextColor="#f65a44"
+                rightText="筛选"
+                :borderBottom="true")
+              q-code(type="html").
+                &ltq-head-bar
+                  :leftArrow="true"
+                  centerText="订单列表"
+                  rightText="筛选"
+                  :borderBottom="true"&gt&lt/q-head-bar&gt
+            q-row(tag="section")
+              q-head-bar(
+                bkColor="#f65a44"
+                color="white"
+                :leftEmpty="true"
+                centerText="购物车"
+                rightText="清空")
+              q-code(type="html").
+                &ltq-head-bar
+                  bkColor="#f65a44"
+                  color="white"
+                  :leftEmpty="true"
+                  centerText="购物车"
+                  rightText="清空"&gt
+                &lt/q-head-bar&gt
+          //- 插槽
+          q-row(tag="section")
+            q-cell(
+              leftIcon="q-icon icon-edit"
+              leftIconColor="deepskyblue"
+              title="插槽")
 </template>
 
 <script lang="ts">
