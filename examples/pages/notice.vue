dc28953f5c2ad680f3a06e4dc130a3b7838ddfb5
Qymh <415570450@qq.com>
2018-07-12 17:17:31 +0800

    add documents

--- examples/pages/notice.vue
+++ examples/pages/notice.vue
@@ -24,12 +24,12 @@
             leftIcon="q-icon icon-edit"
             leftIconColor="deepskyblue"
             title="提醒")
-          q-row(lh=9 indent=2 mb=2) 提醒有三个参数<br>
+          q-row(fontSize=14 lh=9 indent=2 mb=2) 提醒有三个参数<br>
             |第一个指提醒的文字<br>
             |第二个指提醒的位置<main>center|bottom</main>,默认为<main>bottom</main><br>
             |第三个指提醒的时长,单位ms,默认1500
           q-row(tag="section")
-            q-row(@clicked="toast") 点击我触发提醒
+            q-row(@clicked="toast" decoration="underline" color="deepskyblue") 点击我触发提醒
             q-code(type="html").
               &ltq-row @clicked="toast"&gt点击我触发提醒&lt/q-row&gt
 
@@ -45,7 +45,7 @@
             leftIconColor="deepskyblue"
             title="加载")
           q-row(tag="section")
-            q-row(@clicked="loading") 点击我触发加载,1秒后加载消失
+            q-row(@clicked="loading" decoration="underline" color="deepskyblue") 点击我触发加载,1秒后加载消失
             q-code(type="html").
               &ltq-row @clicked="loading"&gt点击我触发加载,1秒后加载消失&lt/q-row&gt
 
@@ -63,12 +63,12 @@
             leftIcon="q-icon icon-edit"
             leftIconColor="deepskyblue"
             title="弹窗")
-          q-row(lh=9 indent=2 mb=2) 弹窗有三个参数<br>
+          q-row(fontSize=14 lh=9 indent=2 mb=2) 弹窗有三个参数<br>
             |第一个指弹窗的文字<br>
             |第二个指弹窗左侧的文字,默认为取消<br>
             |第三个指弹窗右侧的文字,默认为确认
           q-row(tag="section")
-            q-row(@clicked="confirm") 点击我触发弹窗
+            q-row(@clicked="confirm" decoration="underline" color="deepskyblue") 点击我触发弹窗
             q-code(type="html").
               &ltq-row @clicked="confirm"&gt点击我触发弹窗&lt/q-row&gt
               methods:{
@@ -78,6 +78,51 @@
                   })
                 }
               }
+      q-row(tag="section")
+        q-cell(
+          leftIcon="q-icon icon-tagfill"
+          leftIconColor="deepskyblue"
+          title="API一览"
+          fontSize=16
+          :borderBottom="true")
+        //- Api
+        q-row(tag="section")
+          //- toast 
+          q-row(tag="section")
+            q-cell(
+              leftIcon="q-icon icon-edit"
+              leftIconColor="deepskyblue"
+              title="$notice.toast(msg,position,timeout)")
+            q-row(fontSize=14 lh=9 indent=2 mb=2)
+              |<main>msg</main>提示的信息<br>
+              |<main>position</main>提示位置,一个中间center,一个底部bottom,默认为底部bottom<br>
+              |<main>timeout</main>提示的时长,单位ms,默认1500
+          //- loading
+          q-row(tag="section")
+            q-cell(
+              leftIcon="q-icon icon-edit"
+              leftIconColor="deepskyblue"
+              title="$notice.loading")
+            q-row(fontSize=14 lh=9 indent=2 mb=2) 无参数
+          //- removeLoading
+          q-row(tag="section")
+            q-cell(
+              leftIcon="q-icon icon-edit"
+              leftIconColor="deepskyblue"
+              title="$notice.removeLoading")
+            q-row(fontSize=14 lh=9 indent=2 mb=2) 无参数
+          //- confirm
+          q-row(tag="section")
+            q-cell(
+              leftIcon="q-icon icon-edit"
+              leftIconColor="deepskyblue"
+              title="$notice.confirm(msg,btnLeft,btnRight)")
+            q-row(fontSize=14 lh=9 indent=2 mb=2)
+              |<main>msg</main>弹窗信息<br>
+              |<main>btnLeft</main>提示左侧按键的文字 默认为取消<br>
+              |<main>btnRight</main>提示右侧按键的文字 默认为确认
+              |<main>$confirm().then()</main>点击确认后的回调
+            
 </template>
 
 <script lang="ts">
